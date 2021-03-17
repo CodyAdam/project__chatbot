f475f3cc5586c18b459afd2b1c06d757a42b3002
Zoe <zoe.costan@orange.fr>
2021-03-17 11:37:28 +0100

    ajout city

--- src/machine/XMLImporter.scala
+++ src/machine/XMLImporter.scala
@@ -5,13 +5,15 @@ case class Place (
   name: String,
   address: String,
   zipcode: String,
+  city: String,
   phoneNumber : String
 )
 
 object XMLImporter extends App{
 	val xml = XML.loadFile("doc/vAr.xml")
   
-  val test :  Place = Place("dhawhudw", "342","dhawhudw", "342")
-  print(test.name)
+  val test :  Place = Place("Gare", "Rue de la Gare","35577", "Cesson-Sévigné", "06450098")
+  print(test.city)
 }
+
 
