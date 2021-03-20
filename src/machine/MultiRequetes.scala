9f23fde5c27f1092581fcbdfbbcdd8fb14babe5d
CodyAdam <cody.ad4@gmail.com>
2021-03-20 21:13:40 +0100

    Implement the render of the multiRequest results

--- src/machine/MultiRequetes.scala
+++ src/machine/MultiRequetes.scala
@@ -1,5 +1,16 @@
 package machine
 
-class MultiRequetes {
+object MultiRequetes {
   
+  private var stagedResults : Array[Place] = Array[Place]()
+  
+  def formatResults(placesFound : List[Place]) : List[String] = {
+    var result : List[String] = List[String](s"J'ai ${placesFound.length} réponses possibles :" ) //TODO ADD languages
+    stagedResults = placesFound.toArray
+    for(i : Int <- 0 to placesFound.length-1){
+      // example : "1) Piscine Bréquigny"
+      result ++=  List(s"${i+1}) ${placesFound(i).name}")
+    }
+    return result
+  }
 }
