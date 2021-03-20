f25760cc9cf9b864ed45b67c57892d220bb48677
CodyAdam <cody.ad4@gmail.com>
2021-03-20 20:02:12 +0100

    Add init() function that launch at the start of the program

--- src/machine/BaseDonnees.scala
+++ src/machine/BaseDonnees.scala
@@ -4,27 +4,34 @@ import scala.io.Source
 
 object BaseDonnees {
 
-  private var places: List[Place] = List(
-    Place("Mairie de Rennes", "La mairie de Rennes, aussi nommée hôtel de ville de Rennes, désigne à la fois le bâtiment et l'administration et les élus municipaux qui l'occupe",
-      "metropole.rennes.fr", "Place de la Mairie", "35031", "Rennes", "02 23 62 10 10"),
-    Place("Théâtre La Paillette", "La Paillette est une Maison des Jeunes et de la Culture. C’est un lieu de création et de pratiques artistiques. On y va ou on s’y retrouve pour pratiquer une activité, assister à des spectacles, participer à des aventures artistiques, s’assoir dans le coin lecture ou tout simplement se rencontrer.",
-      "la-paillette.net", "2, Rue du Pré de Bris", "35000", "Rennes", "02 99 59 88 86"),
-    Place("Théâtre National de Bretagne", "Le théâtre national de Bretagne (TNB) est une institution culturelle créé à Rennes en 1990 par la réunion du centre dramatique de l'Ouest et de la maison de la Culture de Rennes",
-      "t-n-b.fr", "1, Rue Saint-Hélier", "35040", "Rennes", "02 99 31 12 31"),
-    Place("Gare SNCF", "La gare de Rennes est une gare ferroviaire française de la ligne de Paris-​Montparnasse à Brest, située au sud du centre-ville de Rennes",
-      "gares-sncf.com/fr/gare/frrns/rennes", "19, Place de la Gare", "35005", "Rennes", "36 35"))
-
-  addPlaces(XMLImporter.getPlacesFromXml())
+  private var places: List[Place] = List[Place]()
 
   private val international = Source.fromFile("doc/international.txt").getLines.toList
 
-  private val alias: Map[List[String], List[String]] = AliasImporter.getAliasFromFile()
+  private var alias: Map[List[String], List[String]] = Map[List[String], List[String]]()
 
   /**
-   * Ajoute des lieux à la liste actuel de lieux
-   * @param la liste de Place qui va etre ajouté dans la base de donnees
+   * Initialise la base de donnes avec les alias et les lieux
    */
-  def addPlaces(places: List[Place]): Unit = this.places ++= places;
+  def init(): Unit = {
 
+    //Ajoute les places par défaut
+    places = List(
+      Place("Mairie de Rennes", "La mairie de Rennes, aussi nommée hôtel de ville de Rennes, désigne à la fois le bâtiment et l'administration et les élus municipaux qui l'occupe",
+        "metropole.rennes.fr", "Place de la Mairie", "35031", "Rennes", "02 23 62 10 10"),
+      Place("Théâtre La Paillette", "La Paillette est une Maison des Jeunes et de la Culture. C’est un lieu de création et de pratiques artistiques. On y va ou on s’y retrouve pour pratiquer une activité, assister à des spectacles, participer à des aventures artistiques, s’assoir dans le coin lecture ou tout simplement se rencontrer.",
+        "la-paillette.net", "2, Rue du Pré de Bris", "35000", "Rennes", "02 99 59 88 86"),
+      Place("Théâtre National de Bretagne", "Le théâtre national de Bretagne (TNB) est une institution culturelle créé à Rennes en 1990 par la réunion du centre dramatique de l'Ouest et de la maison de la Culture de Rennes",
+        "t-n-b.fr", "1, Rue Saint-Hélier", "35040", "Rennes", "02 99 31 12 31"),
+      Place("Gare SNCF", "La gare de Rennes est une gare ferroviaire française de la ligne de Paris-​Montparnasse à Brest, située au sud du centre-ville de Rennes",
+        "gares-sncf.com/fr/gare/frrns/rennes", "19, Place de la Gare", "35005", "Rennes", "36 35"))
+
+    places = XMLImporter.getPlacesFromXml()
+    alias = AliasImporter.getAliasFromFile()
+  }
+
+  def findByKeyword(keyword: String): List[Place] = {
+    return places
+  }
   def getAlias(): Map[List[String], List[String]] = alias;
 }
