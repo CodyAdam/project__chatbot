a66340a6fb805f88caf4d64074cb444d12c9bf39
CodyAdam <cody.ad4@gmail.com>
2021-03-20 21:12:42 +0100

    Created Language importer object

---
+++ src/machine/LanguageImporter.scala
@@ -0,0 +1,17 @@
+package machine
+import scala.io.Source
+
+/**
+ * Objet qui converti un fichier text contenant des expression des langue et formate le tout dans des objets adapté
+ * 
+ * pour chaque langue créer un objet Language (définition dans le fichier stateManager.scala)
+ */
+object LanguageImporter {
+  
+  private val dataRaw: List[String] = Source.fromFile("doc/alias.txt").getLines.toList;
+  
+  //TODO 
+  
+  def getLanguagesFromFile() : List[Language] =  ???
+  
+}
