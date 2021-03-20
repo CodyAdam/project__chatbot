39e8185933fe0d4b1d652143f1774f3f39176325
CodyAdam <cody.ad4@gmail.com>
2021-03-20 12:48:33 +0100

    Finished the XMLImporter script

--- src/machine/XMLImporter.scala
+++ src/machine/XMLImporter.scala
@@ -1,19 +1,38 @@
 package machine
-import scala.xml.XML
+import scala.xml
 
-case class Place (
-  name: String,
-  address: String,
-  zipcode: String,
-  city: String,
-  phoneNumber : String
-)
+case class Place(
+  name:        String,
+  description: String,
+  website:     String,
+  address:     String,
+  zipcode:     String,
+  city:        String,
+  phoneNumber: String)
 
-object XMLImporter extends App{
-	val xml = XML.loadFile("doc/vAr.xml")
-  
-  val test :  Place = Place("Gare", "Rue de la Gare","35577", "Cesson-Sévigné", "06450098")
-  print(test.city)
+/**
+ * Objet qui importe les donnees des organisation de la ville de Rennes dans notre Base de donnees
+ */
+object XMLImporter extends App {
+  val xmlData = xml.XML.loadFile("doc/vAr.xml")
+
+  var places: List[Place] = List()
+  (xmlData \\ "organization").map(
+    orga => (orga \\ "address").map(address => {
+      if ((address \ "city").text == "Rennes") {
+        val addressString: String = if (((address \ "street") \ "number").contains(xml.Text)) ((address \ "street") \ "number").text + ", " + ((address \ "street") \ "name").text else ((address \ "street") \ "name").text
+        places = places ++ List(Place(
+          (orga \ "name").text,
+          (orga \ "description").text,
+          (orga \ "web").text,
+          addressString,
+          (address \ "zipcode").text,
+          (address \ "city").text,
+          (address \ "phone").text))
+      }
+    }))
+
+  BaseDonnees.addPlaces(places);
 }
 
 
