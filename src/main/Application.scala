6646dc0628951e1c31908c52ee33617f4f9f3a40
CodyAdam <cody.ad4@gmail.com>
2021-03-21 02:14:33 +0100

    Add comment todo

--- src/main/Application.scala
+++ src/main/Application.scala
@@ -3,9 +3,10 @@ package main
 import scala.swing.SimpleSwingApplication
 import scala.swing.MainFrame
 import ui.UI
+//TODO implement the TTS functionnality
 
 object Application extends SimpleSwingApplication {
 
   machine.DataBase.init()
-  def top : MainFrame = UI 
+  def top: MainFrame = UI
 }
