032bbad0091bae7c17d6333875fec31a51bbc8b6
CodyAdam <cody.ad4@gmail.com>
2021-03-21 01:25:09 +0100

    Add getAddress() function for individual places

--- src/machine/MultiRequetes.scala
+++ src/machine/MultiRequetes.scala
@@ -2,16 +2,20 @@ package machine
 
 object MultiRequetes {
 
-  private var stagedResults: Array[Place] = Array[Place]()
-
+  var stagedResults: Array[Place] = Array[Place]()
+  
   def formatMultiResults(placesFound: List[Place]): List[String] = {
     var result: List[String] = List[String](
-      stateManager.currentLanguage.expression.chose.replace("XXX", placesFound.length.toString()))
+      s"${StateManager.currentLanguage.expression.foundPossibles.replace("XXX", placesFound.length.toString)} :")
     stagedResults = placesFound.toArray
     for (i: Int <- 0 to placesFound.length - 1) {
       // example : "1) Piscine Bréquigny"
       result ++= List(s"${i + 1}) ${placesFound(i).name}")
     }
     return result
+  }
+  
+  def getAddress(place : Place ) : List[String] = {
+    List(s"${StateManager.currentLanguage.expression.address.replace("XXX", place.name)} : ${place.address}")
   }
 }
