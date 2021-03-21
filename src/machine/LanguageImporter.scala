c6a6530bff7c32625729d37cb3b2a6ae1244a913
CodyAdam <cody.ad4@gmail.com>
2021-03-21 02:14:27 +0100

    Test change

--- src/machine/LanguageImporter.scala
+++ src/machine/LanguageImporter.scala
@@ -12,76 +12,76 @@ object LanguageImporter {
    * @return languages in this order as list : français, anglais, espagnol, allemand, italien
    */
   def getLanguages(): List[Language] = {
-      val francais = new Language(
-    "Français",
-    List("bonjour", "salut", "bonsoir"),
-    List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
-    List("restaurant", "creperie", "pizzeria"),
-    new Expression(
-      "oui",
-      "non",
-      "L'adresse de XXX est",
-      "Je ne comprends pas votre demande",
-      "Parlez-vous français?",
-      "D'accord, quelle est votre demande?",
-      "J'ai XXX réponses possibles",
-      "Quel est votre choix?"))
-  val anglais = new Language(// TODO replace with correct language
-    "Français",
-    List("bonjour", "salut", "bonsoir"),
-    List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
-    List("restaurant", "creperie", "pizzeria"),
-    new Expression(
-      "oui",
-      "non",
-      "L'adresse de XXX est",
-      "Je ne comprends pas votre demande",
-      "Parlez-vous français?",
-      "D'accord, quelle est votre demande?",
-      "J'ai XXX réponses possibles",
-      "Quel est votre choix?"))
-  val espagnol = new Language(// TODO replace with correct language
-    "Français",
-    List("bonjour", "salut", "bonsoir"),
-    List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
-    List("restaurant", "creperie", "pizzeria"),
-    new Expression(
-      "oui",
-      "non",
-      "L'adresse de XXX est",
-      "Je ne comprends pas votre demande",
-      "Parlez-vous français?",
-      "D'accord, quelle est votre demande?",
-      "J'ai XXX réponses possibles",
-      "Quel est votre choix?"))
-  val allemand = new Language(// TODO replace with correct language
-    "Français",
-    List("bonjour", "salut", "bonsoir"),
-    List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
-    List("restaurant", "creperie", "pizzeria"),
-    new Expression(
-      "oui",
-      "non",
-      "L'adresse de XXX est",
-      "Je ne comprends pas votre demande",
-      "Parlez-vous français?",
-      "D'accord, quelle est votre demande?",
-      "J'ai XXX réponses possibles",
-      "Quel est votre choix?"))
-  val italien = new Language( // TODO replace with correct language
-    "Français",
-    List("bonjour", "salut", "bonsoir"),
-    List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
-    List("restaurant", "creperie", "pizzeria"),
-    new Expression(
-      "oui",
-      "non",
-      "L'adresse de XXX est",
-      "Je ne comprends pas votre demande",
-      "Parlez-vous français?",
-      "D'accord, quelle est votre demande?",
-      "J'ai XXX réponses possibles",
-      "Quel est votre choix?"))
-    List(francais, anglais, espagnol, allemand, italien) 
+    val francais = new Language(
+      "Français",
+      List("bonjour", "salut", "bonsoir"),
+      List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
+      List("restaurant", "creperie", "pizzeria"),
+      new Expression(
+        "oui",
+        "non",
+        "L'adresse de XXX est",
+        "Je ne comprends pas votre demande",
+        "Parlez-vous français?",
+        "D'accord, quelle est votre demande?",
+        "J'ai XXX réponses possibles",
+        "Quel est votre choix?"))
+    val anglais = new Language( // TODO replace with correct language
+      "Anglais",
+      List("hi", "hello", "morning", "evening", "afternoon", "hey"),
+      List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
+      List("restaurant", "creperie", "pizzeria"),
+      new Expression(
+        "oui",
+        "non",
+        "L'adresse de XXX est",
+        "Je ne comprends pas votre demande",
+        "Parlez-vous français?",
+        "D'accord, quelle est votre demande?",
+        "J'ai XXX réponses possibles",
+        "Quel est votre choix?"))
+    val espagnol = new Language( // TODO replace with correct language
+      "Français",
+      List("bonjour", "salut", "bonsoir"),
+      List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
+      List("restaurant", "creperie", "pizzeria"),
+      new Expression(
+        "oui",
+        "non",
+        "L'adresse de XXX est",
+        "Je ne comprends pas votre demande",
+        "Parlez-vous français?",
+        "D'accord, quelle est votre demande?",
+        "J'ai XXX réponses possibles",
+        "Quel est votre choix?"))
+    val allemand = new Language( // TODO replace with correct language
+      "Français",
+      List("bonjour", "salut", "bonsoir"),
+      List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
+      List("restaurant", "creperie", "pizzeria"),
+      new Expression(
+        "oui",
+        "non",
+        "L'adresse de XXX est",
+        "Je ne comprends pas votre demande",
+        "Parlez-vous français?",
+        "D'accord, quelle est votre demande?",
+        "J'ai XXX réponses possibles",
+        "Quel est votre choix?"))
+    val italien = new Language( // TODO replace with correct language
+      "Français",
+      List("bonjour", "salut", "bonsoir"),
+      List("recherche", "cherche", "ou", "est", "donc", "trouve", "trouver"),
+      List("restaurant", "creperie", "pizzeria"),
+      new Expression(
+        "oui",
+        "non",
+        "L'adresse de XXX est",
+        "Je ne comprends pas votre demande",
+        "Parlez-vous français?",
+        "D'accord, quelle est votre demande?",
+        "J'ai XXX réponses possibles",
+        "Quel est votre choix?"))
+    List(francais, anglais) //TODO replace with : List(francais, anglais, espagnol, allemand, italien)
   }
 }
