c9397733055764f9bdd400dd5958580ec5c2dbbb
CodyAdam <cody.ad4@gmail.com>
2021-03-23 20:09:25 +0100

    add icons

--- src/main/scala/ui/FrameButton.scala
+++ src/main/scala/ui/FrameButton.scala
@@ -1,9 +1,15 @@
 package ui
 import scala.swing._
 
-class FrameButton(label: String, onClick: () => Unit) extends BorderPanel {
+class FrameButton(pathDark: String, pathLight: String, onClick: () => Unit) extends BorderPanel {
 
-  val button = new Button(label) {
+  val imgDark = new Img(pathDark, 10, 10);
+  val imgLight = new Img(pathLight, 10, 10);
+
+  val button = new Button("") {
+    preferredSize = new Dimension(60, 10)
+
+    icon = if (Theme.color == Theme.darkTheme) imgDark.imageIcon else imgLight.imageIcon;
     font = Theme.font.deriveFont(14f);
     background = Theme.color.HIGHLIGHT
     foreground = Theme.color.TEXT
@@ -11,9 +17,9 @@ class FrameButton(label: String, onClick: () => Unit) extends BorderPanel {
     border = new javax.swing.border.EmptyBorder(20, 20, 20, 20)
     listenTo(mouse.moves)
     reactions += {
-      case event.MouseEntered(_,_,_) => background = Theme.color.HIGHLIGHT.brighter()
-      case event.MouseExited(_,_,_) => background = Theme.color.HIGHLIGHT
-      case event.ButtonClicked(_) => onClick()
+      case event.MouseEntered(_, _, _) => background = Theme.color.HIGHLIGHT.brighter()
+      case event.MouseExited(_, _, _)  => background = Theme.color.HIGHLIGHT
+      case event.ButtonClicked(_)      => onClick()
     }
   }
 
@@ -23,6 +29,7 @@ class FrameButton(label: String, onClick: () => Unit) extends BorderPanel {
   reactions += {
     case Theme.ThemeChange =>
       {
+        button.icon = if (Theme.color == Theme.darkTheme) imgDark.imageIcon else imgLight.imageIcon;
         button.background = Theme.color.HIGHLIGHT
         button.foreground = Theme.color.TEXT
       }
