eec60066a0e223cff27b1cbf7335f8e61ea99483
CodyAdam <cody.ad4@gmail.com>
2021-03-23 12:15:12 +0100

    Solve Coonflict

--- src/main/scala/ui/FrameButton.scala
+++ src/main/scala/ui/FrameButton.scala
@@ -1,7 +1,6 @@
 package ui
 import scala.swing._
 
-<<<<<<< HEAD
 class FrameButton(label: String) extends Button(label) {
   background = Theme.color.HIGHLIGHT
   foreground = Theme.color.MAIN
@@ -15,10 +14,4 @@ class FrameButton(label: String) extends Button(label) {
         foreground = Theme.color.MAIN
       }
   }
-=======
-class FrameButton(label : String) extends Button(label){
-    background = Theme.color.HIGHLIGHT
-    foreground = Theme.color.MAIN
-    border = new javax.swing.border.EmptyBorder(5, 15, 5, 15)
->>>>>>> branch 'master' of https://gitlab.istic.univ-rennes1.fr/arallain/avatar_project.git
 }
