55901b893e897de51865316fd7ea3d8a740b2171
CodyAdam <cody.ad4@gmail.com>
2021-03-26 01:28:45 +0100

    Fix TTS

--- src/main/scala/ui/TextBubble.scala
+++ src/main/scala/ui/TextBubble.scala
@@ -5,7 +5,6 @@ import javax.swing.SwingConstants
 
 class TextBubble(lang: machine.Language, msg: String, isUser: Boolean, parentWidth: Int) extends Button("<html>" + msg) {
 
-  
   font = Theme.fontLight.deriveFont(20f)
   opaque = true
   foreground = Theme.color.TEXT
@@ -21,7 +20,10 @@ class TextBubble(lang: machine.Language, msg: String, isUser: Boolean, parentWid
   if (maxWidth != 0)
     line = math.ceil(textWidth / maxWidth).toInt + 1
 
-  //maximumSize = new Dimension(maxWidth, 999999)
+
   preferredSize = new Dimension(0, lineHeight * line);
 
+  reactions += {
+    case event.ButtonClicked(_) => machine.TextToSpeech.speak(msg, lang)
+  }
 }
