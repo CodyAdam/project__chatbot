02fd91f2f245c7ab530d1fb2d7e08ac9d20c7c96
Zoe <zoe.costan@orange.fr>
2021-03-29 20:06:11 +0200

    test blagues modifiés

--- src/test/scala/testMachine/CaseBlague.scala
+++ src/test/scala/testMachine/CaseBlague.scala
@@ -23,91 +23,45 @@ class CaseBlague {
    * - demander une blague avec les mots de la langue actue
    */
 
-// Français :
+  // Français :
 
   @Test
-  def ask(){
-     StateManager.currentLanguage = DataBase.getLanguages()(0)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("blague")))
+  def ask() {
+    StateManager.currentLanguage = DataBase.getLanguages()(0)
+    assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
   }
- 
-  @Test
-  def ask13(){
-     StateManager.currentLanguage = DataBase.getLanguages()(0)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("plasanterie")))
-  }
- 
-// Anglais
 
-  
-  @Test
-  def ask2(){
-     StateManager.currentLanguage = DataBase.getLanguages()(1)
-      assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("joke")))
-  }
-   
-  @Test
-  def ask12(){
-     StateManager.currentLanguage = DataBase.getLanguages()(1)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("jokes")))
-  }
-   
-   // Espagnol
+  // Anglais
 
   @Test
-  def ask3(){
-     StateManager.currentLanguage = DataBase.getLanguages()(2)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("broma")))
+  def ask2() {
+    StateManager.currentLanguage = DataBase.getLanguages()(1)
+    assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
+    assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
+    assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
   }
-   
+
+  // Espagnol
+
   @Test
-  def ask10(){
-     StateManager.currentLanguage = DataBase.getLanguages()(2)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("chanca")))
+  def ask3() {
+    StateManager.currentLanguage = DataBase.getLanguages()(2)
+    assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
   }
-  
-  @Test
-  def ask11(){
-     StateManager.currentLanguage = DataBase.getLanguages()(2)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("chistes")))
+
+    // Allemand
+
+    @Test
+    def ask4() {
+      StateManager.currentLanguage = DataBase.getLanguages()(3)
+      assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
+    }
+
+    // Italien
+    @Test
+    def ask5() {
+      StateManager.currentLanguage = DataBase.getLanguages()(4)
+      assertTrue(DataBase.getJokesCurrentLang.list.contains(DataBase.getJoke()))
+    }
+
   }
-   
-   // Allemand
-                                       
-  @Test
-  def ask4(){
-     StateManager.currentLanguage = DataBase.getLanguages()(3)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("scherz")))
-  }
-   
-     @Test
-  def ask8(){
-     StateManager.currentLanguage = DataBase.getLanguages()(3)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("wits")))
-  }
-     
-       @Test
-  def ask9(){
-     StateManager.currentLanguage = DataBase.getLanguages()(3)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("straich")))
-  }
-   
-   // Italien
-  @Test
-  def ask5(){
-     StateManager.currentLanguage = DataBase.getLanguages()(4)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("barzelletta")))
-  }
-   
-     @Test
-  def ask6(){
-     StateManager.currentLanguage = DataBase.getLanguages()(4)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("Storiela")))
-  }
-     
-       @Test
-  def ask7(){
-     StateManager.currentLanguage = DataBase.getLanguages()(4)
-    assertTrue(DataBase.getJokesCurrentLang.list.contains(MachineImpl.ask("batutà")))
-  }
-}
