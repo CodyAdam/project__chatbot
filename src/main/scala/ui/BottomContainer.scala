fc5b3b093f0e428776bae35aed2b8a1ca6151e49
CodyAdam <cody.ad4@gmail.com>
2021-03-30 00:41:27 +0200

    Rounded imple

--- src/main/scala/ui/BottomContainer.scala
+++ src/main/scala/ui/BottomContainer.scala
@@ -1,18 +1,13 @@
 package ui
+
 import scala.swing._
 import scala.swing.event._
 import java.awt.Color
 import scala.io.StdIn._
 
 object BottomContainer extends BoxPanel(Orientation.Horizontal) {
-  val sendButton = new Button(">") {
-    preferredSize = new Dimension(44, 50)
-    background = Theme.color.SECONDARY
-  }
-  val vttButton = new Button("startRecognition") {
-    preferredSize = new Dimension(150, 50)
-    background = Theme.color.SECONDARY
-  }
+  val sendButton = new ui.RoundButton("assets/kiwi-icon.png")
+  val vttButton = new ui.RoundButton("assets/kiwi-icon.png")
 
   listenTo(sendButton, vttButton, Theme)
   reactions += {
@@ -22,10 +17,6 @@ object BottomContainer extends BoxPanel(Orientation.Horizontal) {
       else if (button == vttButton)
         machine.SpeechToText.startRecognition()
     }
-    case Theme.ThemeChange =>
-      {
-        sendButton.background = Theme.color.SECONDARY
-      }
   }
 
   border = new javax.swing.border.EmptyBorder(10, 10, 10, 10)
