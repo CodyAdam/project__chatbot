84b10153086a6e6c05d1255d428250bf8c80ea43
mgallou <mael.gallou.1@etudiant.univ-rennes1.fr>
2021-03-30 22:21:33 +0200

    dernier test casePolite mael

--- src/test/scala/testMachine/CasePolite.scala
+++ src/test/scala/testMachine/CasePolite.scala
@@ -28,16 +28,37 @@ class CasePolite {
   def politesse1() = {
     assertEquals(MachineImpl.ask("Bonjour"), List("Bonjour"))
   }
+  
+  //avec un mot de politesse de la langue actuel
   @Test
   def politesse2() = {
-    assertEquals(MachineImpl.ask("Morgen"), List("Sprechen Sie Deutsch?"))
+    assertEquals(MachineImpl.ask("Salut"), List("Bonjour"))
   }
   
-  //avec un mot de politesse de la langue actuel et d'autres mots
+  //avec un mot de politesse de la langue actuel et un mot incompris par le robot
   @Test
   def politesse3() = {
-    assertEquals(MachineImpl.ask("Bonjour agaghdhhjd"), List("Bonjour","Je ne comprends pas votre demande"))
-    assertEquals(MachineImpl.ask("Joke"), List("Do you speak english?"))
+    assertEquals(MachineImpl.ask("Bonjour boss"), List("Bonjour","Je ne comprends pas votre demande"))
   }
   
+  //avec un mot de politesse d'une autre langue
+  @Test
+  def politesse4() = {
+    assertEquals(MachineImpl.ask("Hello"), List("Do you speak english?"))
+  }
+  
+  //avec un mot de politesse dans la langue déjà modifiée
+  @Test
+  def politesse5() = {
+    StateManager.changeLanguage(DataBase.getLanguages()(1))
+    assertEquals(MachineImpl.ask("Hello"), List("Hi"))
+  }
+  
+  //avec un mot de politesse dans la langue déjà modifiée et un mot incompris par le robot
+  @Test
+  def politesse6() = {
+    StateManager.changeLanguage(DataBase.getLanguages()(1))
+    assertEquals(MachineImpl.ask("Hello frero"), List("Hi","I do not understand"))
+  }
+
 }
