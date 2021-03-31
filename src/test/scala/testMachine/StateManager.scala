c481976aaa2392a8a8b034efc12aa3ff40378d25
Thomega35 <61747645+Thomega35@users.noreply.github.com>
2021-03-31 03:03:57 +0200

    StateManager.getNextLanguage() test finish (switch language

--- src/test/scala/testMachine/StateManager.scala
+++ src/test/scala/testMachine/StateManager.scala
@@ -19,8 +19,41 @@ class StateManager {
   /*
    Cas à tester :
 
-   - si le currentLanguage est le premier de la liste rend bien le second de la liste
-   - si current Language est le dernier de la liste rend bien le premier de la liste
-   - si l'ordre est bien français, anglais, espagnol, allemand, italien, puis de nouveau français, anglais...
+   - quand le currentLanguage est le premier de la liste, rend bien le second de la liste
+   - quand current Language est le dernier de la liste, rend bien le premier de la liste
+   - l'ordre est bien français, anglais, espagnol, allemand, italien, puis de nouveau français, anglais...
   */
+  
+  //quand le currentLanguage est le premier de la liste, rend bien le second de la liste
+  @Test
+  def testgetNextLanguage1{
+    StateManager.currentLanguage = DataBase.getLanguages()(0)
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage, DataBase.getLanguages()(1))
+  }
+  
+  //quand current Language est le dernier de la liste, rend bien le premier de la liste
+  @Test
+  def testgetNextLanguage2{
+    StateManager.currentLanguage = DataBase.getLanguages()(DataBase.getLanguages().length-1)
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage, DataBase.getLanguages()(0))
+  }
+  
+  //l'ordre est bien français, anglais, espagnol, allemand, italien, puis de nouveau français, anglais...
+  @Test
+  def testgetNextLanguage3{
+    StateManager.currentLanguage = DataBase.getLanguages()(0)
+    assertEquals(StateManager.currentLanguage.langue, "Français")
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage.langue, "English")
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage.langue, "Español")
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage.langue, "Deustch")
+    StateManager.currentLanguage = StateManager.getNextLanguage()
+    assertEquals(StateManager.currentLanguage.langue, "Italiano")
+    StateManager.currentLanguage = StateManager.getNextLanguage()//retour au debut
+    assertEquals(StateManager.currentLanguage.langue, "Français")
+  }
 }
